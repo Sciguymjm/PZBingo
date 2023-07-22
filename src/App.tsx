4f63061b8eb6662802480095928b3ec13ffcaa60
Matthew Mage <sciguymjm@gmail.com>
2023-07-22 14:55:32 -0700

    Adds overlays and different seeds/areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import MISSIONS from './missions.json';
+import OVERLAYS from './overlays.json';
+
+const MISSIONS_TOTAL = MISSIONS;
+
+// add "Clear <location>" missions
 
 // create a random generator with a seed
 // https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
@@ -22,6 +27,7 @@ function sample<T>(rng: () => number, arr: T[]): T {
 interface Mission {
   name: string;
   description: string;
+  link?: string;
 }
 
 interface BingoProps {
@@ -33,14 +39,22 @@ interface BingoProps {
 // original <td className="mission">{props.mission.name}</td>
 function BingoCell(props: { mission: Mission }) {
   const [showDescription, setShowDescription] = React.useState(false);
+  const [completed, setCompleted] = React.useState(false);
   return <>
     <td className="mission"
         onMouseEnter={() => setShowDescription(true)}
         onMouseLeave={() => setShowDescription(false)}
+        onClick={() => setCompleted(!completed)}
+        style={{ backgroundColor: completed ? 'green' : 'inherit' }}
     >
       <div className="missionName">{props.mission.name}</div>
-      <div className="missionDescription" style={{ display: showDescription ? 'block' : 'none' }}
-      >{props.mission.description}</div>
+      <div className="missionDescription"
+           onClick={e => e.stopPropagation()}
+           style={{ display: showDescription ? 'block' : 'none' }}
+      >
+        {props.mission.description} {props.mission.link &&
+        <a href={props.mission.link} target="_blank" rel="noreferrer">Map</a>}
+      </div>
     </td>
   </>;
 }
@@ -54,7 +68,7 @@ function BingoBoard(props: BingoProps) {
     for (let j = 0; j < 5; j++) {
       // free space
       if (i === 2 && j === 2) {
-        cols.push(<td key="freeSpace" className="mission">Free Space</td>);
+        cols.push(<td key="freeSpace" className="mission" style={{ backgroundColor: 'green' }}>Free Space</td>);
         continue;
       }
       let mission = sample(prng, props.missions);
@@ -79,17 +93,53 @@ function BingoBoard(props: BingoProps) {
 
 }
 
+const getPOIs = (area: string = 'All') => {
+  // return all of the POI names from OVERLAYS, as well as their PZ map coordinates as a url,
+  // like https://map.projectzomboid.com/#10380x8807x441 where 441 is the zoom level
+  return OVERLAYS.areas.filter(a => area === 'All' || a.name === area)
+    .flatMap(area => area.pois.map(poi => ({
+    name: `${poi.name} (${area.name})`,
+    url: `https://map.projectzomboid.com/#${poi.x}x${poi.y}x2000`
+  })));
+};
+
+const getAreas = () => {
+  return OVERLAYS.areas.map(area => area.name);
+};
 
 function App() {
   const [seed, setSeed] = React.useState(0);
+  const areas = ['All'].concat(getAreas());
+  const [area, setArea] = React.useState(areas[0]);
+  const pois = getPOIs(area);
+  const poiPRNG = mulberry32(seed);
+  // sample 10 random pois
+  const randomPOIs = [];
+  for (let i = 0; i < 10; i++) {
+    const poi = sample(poiPRNG, pois);
+    randomPOIs.push(poi);
+  }
+  // add "Clear <location>" missions to total missions
+  const CLEAR_MISSIONS = randomPOIs.map(poi => ({
+    name: `Clear ${poi.name}`,
+    description: `Clear ${poi.name}`,
+    link: poi.url
+  }));
+  const TOTAL_MISSIONS = MISSIONS.concat(CLEAR_MISSIONS);
   return (
     <div className="App">
       <header className="App-header">
-        <BingoBoard missions={MISSIONS} seed={seed} />
+        <BingoBoard missions={TOTAL_MISSIONS} seed={seed} />
         <div className="seed">
           <label htmlFor="seed">Seed:</label>
           <input type="number" id="seed" value={seed} onChange={e => setSeed(parseInt(e.target.value))} />
           <button onClick={() => setSeed(Math.floor(Math.random() * 1000000000))}>Random</button>
+        </div>
+        <div className={"area"}>
+          <label htmlFor="area">Area:</label>
+          <select id="area" value={area} onChange={e => setArea(e.target.value)}>
+            {areas.map(area => <option key={area} value={area}>{area}</option>)}
+          </select>
         </div>
       </header>
     </div>
